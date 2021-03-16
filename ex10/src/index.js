3fbc93bdf02d7ac3feaf055279ef51677f772324
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-16 21:09:05 +0100

    Created ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line 
+
+// Only change code above this line
+function logicOrder(num){
+    if(num<50){
+        return "Less than 50";
+
+    }else if (num<100){
+        return "Less than 100";
+
+    }else{
+        return "Greater than or equal to 100";
+
+    }
+
+}
+console.log(logicOrder(28));
+console.log(logicOrder(60));
+console.log(logicOrder(110));
+module.exports = logicOrder;
