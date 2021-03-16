a919bed6751e1bfce5a5564dfa910ffc652235d9
Adnan Hamzagic <adnan.hamzagic11@gmail.com>
2021-03-16 17:10:32 +0100

    Created ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+
+// Only change cod above this line
+function myTrueBoolean(){
+    return true;
+    
+}
+
+
+console.log(myTrueBoolean());
+module.exports = myTrueBoolean;
